896d4abf32b358358cd16d1cf7e63fd6e009ae55
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-02-20 20:33:30 +0200

    task solved

--- HTTP&REST-Services/01.Bus-Stop/app.js
+++ HTTP&REST-Services/01.Bus-Stop/app.js
@@ -1,9 +1,53 @@
 function getInfo() {
-    const submitBtn = document.getElementById('submit');
+    // Note: The service will respond with valid data to IDs 1287, 1308, 1327 and 2334.
+    const httpReq = new XMLHttpRequest();
+    const stopId = document.getElementById('stopId');
+    const stopName = document.getElementById('stopName');
+    const buses = document.getElementById('buses');
+    let station = {};
 
-    submitBtn.addEventListener('click', (e) => {
-        e.preventDefault();
+    if (stopId.value) {
+        clearPreviousData();
+        if (!httpReq) {
+            console.warn('Unable to create AJAX instance');
+            return false;
+        }
+    
+        httpReq.open('GET', `http://localhost:3030/jsonstore/bus/businfo/${stopId.value}`, true);
+        httpReq.getResponseHeader('Content-type', 'application/json');
+    
+        httpReq.onload = () => {
+            if (!httpReq.responseText) {
+                stopName.textContent = 'Error';
+            } else {
+                station = JSON.parse(httpReq.responseText);
+                generateDOM();
+            }
+        }
 
-        console.log(e.target);
-    });
+        httpReq.send();
+    } else {
+        console.warn('Invalid / empty ID');
+    }
+
+    function generateDOM() {
+        stopName.textContent = station.name;
+
+        let busesValues = station.buses;
+
+        for (const bus of Object.entries(busesValues)) {
+            let li = document.createElement('li');
+            li.textContent = `Bus ${bus[0]} arrives in ${bus[1]} minutes`;
+            buses.appendChild(li);
+        }
+    }
+    
+    function clearPreviousData() {
+        let child = buses.lastChild;
+
+        while (buses.lastChild) {
+            child.remove();
+            child = buses.lastChild;
+        }
+    }
 }
