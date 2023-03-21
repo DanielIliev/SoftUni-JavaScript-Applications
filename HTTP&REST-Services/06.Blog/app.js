a0fb68d71f406edc5c99a03453d03074dbd6929a
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-03-21 12:15:08 +0200

    Initial commit

--- HTTP&REST-Services/06.Blog/app.js
+++ HTTP&REST-Services/06.Blog/app.js
@@ -1,5 +1,30 @@
 function attachEvents() {
-    console.log('TODO...');
+    // URLs
+    // http://localhost:3030/jsonstore/blog/posts
+    // http://localhost:3030/jsonstore/blog/comments/:id
+
+    const loadPosts = document.getElementById('btnLoadPosts');
+    const postsSelectList = document.getElementById('posts');
+    const viewPostsBtn = document.getElementById('btnViewPost');
+    let posts = {};
+
+    fetch('http://localhost:3030/jsonstore/blog/posts')
+    .then(response => response.json())
+    .then(data => {
+        posts = data;
+        Object.entries(data).forEach((element) => {
+            postsSelectList.innerHTML += `<option value=${element[0]}>${element[1].title}</option>`;
+        });
+    });
+
+    viewPostsBtn.addEventListener('click', () => {
+        console.log(`http://localhost:3030/jsonstore/blog/comments/${postsSelectList.value}`);
+        // fetch(`http://localhost:3030/jsonstore/blog/comments/${postsSelectList.value}`)
+        // .then(response => response)
+        // .then(data => {
+        //     console.log(data);
+        // })
+    });
 }
 
 attachEvents();
