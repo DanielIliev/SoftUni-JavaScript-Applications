6fc2a6030bb239ae0a285d78618947356beb098e
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-03-21 12:59:26 +0200

    task solved

--- Data&Auth/01.Messenger/app.js
+++ Data&Auth/01.Messenger/app.js
@@ -1,5 +1,33 @@
 function attachEvents() {
-    console.log('TODO...');
+    // URLs
+    // http://localhost:3030/jsonstore/messenger
+    // -------------------------------------------
+    const url = 'http://localhost:3030/jsonstore/messenger';
+    const messagesArea = document.getElementById('messages');
+    const authorField = document.querySelector('input[name="author"]');
+    const contentField = document.querySelector('input[name="content"]');
+
+    document.getElementById('submit').addEventListener('click', () => {
+        let msgObj = { 'author': authorField.value, 'content': contentField.value };
+
+        const options = {
+            'method': 'POST',
+            'Content-Type': 'application/json',
+            'body': JSON.stringify(msgObj)
+        }
+
+        fetch(url, options);
+    });
+
+    document.getElementById('refresh').addEventListener('click', () => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                Object.values(data).forEach(message => {
+                    messagesArea.value += `${message.author}: ${message.content}\n`;
+                });
+            })
+    });
 }
 
 attachEvents();
