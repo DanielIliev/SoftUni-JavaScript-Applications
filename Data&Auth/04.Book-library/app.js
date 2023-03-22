6a6b3523d7f76eef085d20b4fb5dcb57da3b82a4
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-03-22 16:56:54 +0200

    tasks documentation (only comments in app.js)

--- Data&Auth/04.Book-library/app.js
+++ Data&Auth/04.Book-library/app.js
@@ -1,1 +1,8 @@
-console.log('My requests...')
+function solve() {
+    // URLs
+    // For GET on all books http://localhost:3030/jsonstore/collections/books
+    // For GET / POST / PUT on specific book http://localhost:3030/jsonstore/collections/books/:id
+
+}
+
+solve();
